08d381bfe4155d801fc8bdd2a3d3917d772f0a18
angular-cli <angular-cli@angular.io>
2018-06-20 23:01:10 -0400

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SteganographyPage } from './app.po';
+
+describe('steganography App', function() {
+  let page: SteganographyPage;
+
+  beforeEach(() => {
+    page = new SteganographyPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
